0162f4b5851a83574a5dfba2ec88d3fc47e563be
adam-belahouane <adam_belahouane@outlook.com>
2021-10-29 09:23:32 +0100

    update

--- src/views/new/index.jsx
+++ src/views/new/index.jsx
@@ -15,20 +15,64 @@ const NewBlogPost = () => {
     comments: [],
     author: {
       name: "AUTHOR AVATAR NAME",
+      email: "",
       avatar: "https://cdn.fakercloud.com/avatars/mhesslow_128.jpg",
     },
     content: "",
   });
+  
+  const[email, setEmail] = useState("")
+
+  const emailNewBlogPost = async () => {
+    try {
+      let response = await fetch(process.env.REACT_APP_BE + "blogPosts/blogPostMade",
+      {
+        method: "POST",
+        body: JSON.stringify(email),
+        "Content-type": "application/json",
+      })
+    } catch (error) {
+      
+    }
+
+  }
+
+  const handleInput = (propertyName, value) => {
+    setNewBlogPost({
+      ...newBlogPost,
+      [propertyName]: value,
+    });
+  };
+
+  const handleInputAuthor = (propertyName, value) => {
+    setNewBlogPost({
+      ...newBlogPost,
+      author: {
+        [propertyName]: value,
+      },
+    });
+  };
+
   return (
     <Container className="new-blog-container">
       <Form className="mt-5">
         <Form.Group controlId="blog-form" className="mt-3">
           <Form.Label>Title</Form.Label>
-          <Form.Control size="lg" placeholder="Title" />
+          <Form.Control
+            size="lg"
+            placeholder="Title"
+            onChange={(e) => handleInput("title", e.target.value)}
+            value={newBlogPost.title}
+          />
         </Form.Group>
         <Form.Group controlId="blog-category" className="mt-3">
           <Form.Label>Category</Form.Label>
-          <Form.Control size="lg" as="select">
+          <Form.Control
+            size="lg"
+            as="select"
+            onChange={(e) => handleInput("category", e.target.value)}
+            value={newBlogPost.category}
+          >
             <option>Category1</option>
             <option>Category2</option>
             <option>Category3</option>
@@ -39,9 +83,27 @@ const NewBlogPost = () => {
         <Form.Group controlId="blog-content" className="mt-3">
           <Form.Label>Blog Content</Form.Label>
           <ReactQuill
-            value={this.state.text}
-            onChange={this.handleChange}
+            value={newBlogPost.content}
+            onChange={(e) => handleInput("content", e)}
             className="new-blog-content"
+          />
+        </Form.Group>
+        <Form.Group controlId="blog-author" className="mt-3">
+          <Form.Label>Author Name</Form.Label>
+          <Form.Control
+            size="lg"
+            placeholder="Name"
+            onChange={(e) => handleInputAuthor("name", e.target.value)}
+            value={newBlogPost.author.name}
+          />
+        </Form.Group>
+        <Form.Group controlId="blog-authoremail" className="mt-3">
+          <Form.Label>Author email</Form.Label>
+          <Form.Control
+            size="lg"
+            placeholder="Name"
+            onChange={setEmail(e.target.value)}
+            value={email}
           />
         </Form.Group>
         <Form.Group className="d-flex mt-3 justify-content-end">
@@ -62,4 +124,4 @@ const NewBlogPost = () => {
   );
 };
 
-export default NewBlogPost
+export default NewBlogPost;
